4eb10fa1a472ebcec8044333ab1820ca3da5d96b
Anjuka Koralage <anjuka.2014022@iit.ac.lk>
2019-04-29 22:37:45 -0700

    Read From File

--- app/src/main/java/com/fyp/anjukakoralage/sinclassify/MainActivity.java
+++ app/src/main/java/com/fyp/anjukakoralage/sinclassify/MainActivity.java
@@ -1,8 +1,12 @@
 package com.fyp.anjukakoralage.sinclassify;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,12 +15,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int REARMISION_REQUEST_STORAGE = 1000;
+    private static final int READ_REQUEST_CODE = 42;
 
-    private Button btnImport;
+    private Button btnImport,btnClear;
     private EditText txtInput;
+    private String fulltext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +37,75 @@ public class MainActivity extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED){
-            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REARMISION_REQUEST_STORAGE);
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REARMISION_REQUEST_STORAGE);
         }
 
         btnImport = (Button) findViewById(R.id.btnImport);
+        btnClear = (Button) findViewById(R.id.btnClear);
         txtInput = (EditText) findViewById(R.id.txtInput);
+
+        ArrayList<String> sentenceList = new ArrayList<String>();
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtInput.setText("");
+                txtInput.setHint(R.string.boxText);
+            }
+        });
 
         btnImport.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                performFileSearch();
 
             }
         });
+    }
+
+    //read content text file
+    private String readText(String input){
+        File file = new File(Environment.getExternalStorageDirectory(), input);
+        StringBuilder text = new StringBuilder();
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = br.readLine()) != null){
+                text.append(line);
+                text.append("\n");
+            }
+            br.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        return text.toString();
+    }
+
+    //select file from storage
+    private void performFileSearch(){
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            if (data != null){
+                Uri uri = data.getData();
+                String path = uri.getPath();
+                path = path.substring(path.indexOf(":") + 1);
+                if (path.contains("emulated")){
+                    path = path.substring(path.indexOf("0") + 1);
+                }
+                Toast.makeText(this, "" + path, Toast.LENGTH_SHORT).show();
+                txtInput.setText(readText(path));
+
+
+            }
+        }
     }
 
     @Override
@@ -47,7 +116,7 @@ public class MainActivity extends AppCompatActivity {
             }
             else {
                 Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show();
-                finish();
+
             }
         }
     }
