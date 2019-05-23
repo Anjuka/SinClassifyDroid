28a02e0b319d656157b42d190f97eda437b4ca70
Anjuka Koralage <anjuka.2014022@iit.ac.lk>
2019-05-23 11:47:28 -0700

    Add Save button to res Activity

--- app/src/main/java/com/fyp/anjukakoralage/sinclassify/ResultActivity.java
+++ app/src/main/java/com/fyp/anjukakoralage/sinclassify/ResultActivity.java
@@ -2,12 +2,29 @@ package com.fyp.anjukakoralage.sinclassify;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class ResultActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResultActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Bundle bundleAdd, bundleResult;
+    private FirebaseAuth firebaseAuth;
+    private CoordinatorLayout coordinatorLayout;
+
     private TextView txtCrime;
     private TextView txtPolitic;
     private TextView txtBusiness;
@@ -19,6 +36,29 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View headerView = navigationView.getHeaderView(0);
+        ImageView ivUserImage = (ImageView) headerView.findViewById(R.id.ivUserImage);
+        TextView tvSalutation = (TextView) headerView.findViewById(R.id.tvSalutation);
+
+        tvSalutation.setText(firebaseAuth.getCurrentUser().getEmail());
+
 
         txtCrime = findViewById(R.id.txtCrime);
         txtPolitic = findViewById(R.id.txtPolitic);
@@ -44,6 +84,51 @@ public class ResultActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.nav_home) {
+            Intent Home = new Intent(getApplicationContext(), MainActivity.class);
+            Home.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(Home);
+        } else if (id == R.id.nav_logout) {
+            firebaseAuth.signOut();
+            Intent out = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(out);
+        } else if (id == R.id.nav_link) {
+            Intent resa = new Intent(getApplicationContext(), ResourcesActivity.class);
+            startActivity(resa);
+        }
+        else if (id == R.id.nav_support) {
+            Intent resa = new Intent(getApplicationContext(), SupportActivity.class);
+            startActivity(resa);
+        }
+        else if (id == R.id.nav_contacUs) {
+            Intent resa = new Intent(getApplicationContext(), ContactUsActivity.class);
+            startActivity(resa);
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.save, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_save) {
+            Snackbar snackbar = Snackbar.make(coordinatorLayout, "Saved Successfully", Snackbar.LENGTH_LONG);
+            snackbar.show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
